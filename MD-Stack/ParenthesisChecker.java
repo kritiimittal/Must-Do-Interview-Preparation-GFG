c08e693e78d16e5bcfd13a427ea4371715147956
Kriti Mittal <31966891+kritiimittal@users.noreply.github.com>
2020-12-11 18:44:12 +0530

    Create ParenthesisChecker.java

---
+++ MD-Stack/ParenthesisChecker.java
@@ -0,0 +1,41 @@
+class Parenthesis
+{
+    // Function to return if the paranthesis are balanced or not
+    static boolean ispar(String x)
+    {
+        
+        if(x.length()==0){
+            return true;
+        }
+        
+        Stack<Character> s=new Stack<>();
+        int i=0;
+        while(i<x.length()){
+            char c=x.charAt(i);
+            if(c=='{'||c=='['||c=='('){
+                s.push(c);
+            }
+            
+            if(c==']'||c=='}'||c==')'){
+                
+            if(s.isEmpty()){
+                return false;
+            }    
+            
+            if(c==']' && s.peek()=='['){
+                s.pop();
+            }else if(c=='}' && s.peek()=='{'){
+                s.pop();
+            }else if(c==')' && s.peek()=='('){
+                s.pop();
+            } 
+                
+            }
+            
+            i++;
+        }
+        
+        return s.isEmpty();
+    }
+    
+}

6b0f9a63c3813c467b6e76b3ecf125b5fa71be47
Kriti Mittal <31966891+kritiimittal@users.noreply.github.com>
2020-12-12 20:33:03 +0530

    Create BinarySearch.java

---
+++ MD-Arrays/BinarySearch.java
@@ -0,0 +1,23 @@
+class Solution {
+    int binarysearch(int arr[], int n, int k){
+       
+       int start=0;
+       int end=n-1;
+       while(start<=end){
+           int mid=(start+end)/2;
+           if(arr[mid]==k){
+               return mid;
+           }else if(k<arr[mid]){
+               end=mid-1;
+           }else{
+               start=mid+1;
+           }
+           
+           
+           
+           
+       }
+       return -1;
+       
+    }
+}
